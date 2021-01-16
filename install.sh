bd04a1cbc5fb276f57a0f959ef537d282c7f5043
happyeggchen <happyegg.chen@hotmail.com>
2021-01-16 16:52:36 +0800

    本次提交包含窗口装饰，图标，桌面外观，壁纸

---
+++ install.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+echo "RTD Warning!"
+echo "This theme is for bluelakelinux prive edition"
+echo "it is highly based on We10Xdark theme on github by yeyushengfan258"
+echo "press any key to copy file into your system"
+echo "-----------"
+echo " About root require:For install icons to your system"
+read
+  cp -r  aurorae/ ~/.local/share
+  cp -r plasma/ ~/.local/share
+  cp -r wallpapers ~/.local/share
+  sudo cp -r Flat-Remix-Green /usr/share/icons
