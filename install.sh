d5edeab6fbdd27078b26eec12fa172198795e40e
happyeggchen <happyegg.chen@hotmail.com>
2021-02-04 22:40:17 +0800

    add tiling script to default , fix sddm button icons

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@ echo "This theme is for bluelakelinux edition"
 echo "it is highly based on We10Xdark theme on github by yeyushengfan258"
 echo "press any key to copy file into your system"
 echo "-----------"
-echo " About root require:For install icons to your system"
+echo " About root require:For install sddm theme , icons to your system"
 read
   mkdir ~/rupm
   mkdir ~/rupm/pkgs
@@ -14,3 +14,5 @@ read
   cp -r kvantum ~/rupm/pkgs
   sudo cp -r Flat-Remix-Green /usr/share/icons
   sudo cp -r sddm/bluelake /usr/share/sddm/themes/
+  plasmapkg2 --type kwinscript -i scripts/kwin-script-tiling/
+  echo done!
