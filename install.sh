e1d2c2003253602f44ac28265b764c488fd70cbd
happyeggchen <happyegg.chen@hotmail.com>
2021-02-11 17:46:05 +0800

    Big updates here,a new install script ,and many things get automatic,add a nightlight plugin and newest layout

--- install.sh
+++ install.sh
@@ -1,18 +1,62 @@
-#!/bin/sh
-echo "RTD Warning!"
-echo "This theme is for bluelakelinux edition"
-echo "it is highly based on We10Xdark theme on github by yeyushengfan258"
-echo "press any key to copy file into your system"
-echo "-----------"
-echo " About root require:For install sddm theme , icons to your system"
-read pause
-  mkdir ~/rupm
-  mkdir ~/rupm/pkgs
-  cp -r  aurorae/ ~/.local/share
-  cp -r plasma/ ~/.local/share
-  cp -r wallpapers ~/.local/share
-  cp -r kvantum ~/rupm/pkgs
-  sudo cp -r Flat-Remix-Green /usr/share/icons
-  sudo cp -r sddm/bluelake /usr/share/sddm/themes/
-  plasmapkg2 --type kwinscript -i scripts/kwin-script-tiling/
-  echo done!
+#!/bin/bash
+    clear
+        #flash_plasma_theme#
+            echo -e "\033[32mWelcom\033[0m \033[34musing\033[0m \033[31mbluelakelinux\033[0m \033[33mflash\033[0m \033[35mscript\033[0m"
+            echo "-------------Plasma Theme Installation--------------"
+            echo "Now we're going to install bluelake kde theme"
+            echo "want to install it into your system , Press enter"
+            echo " Or Ctrl+c to install next part"
+            echo "----------------------------------------------------"
+            read pause
+
+            #installer#
+
+                clear
+
+#Writing_files#
+
+                #banner#
+                echo -e "\033[32mWelcom\033[0m \033[34musing\033[0m \033[31mbluelakelinux\033[0m \033[33mflash\033[0m \033[35mscript\033[0m"
+                #banner_end#
+
+                #progress_bar#
+                echo "[#######-----------------------------------------]"
+                echo "[BluelakeLinuxInstaller]Writing files............."
+                #progress_bar_end#
+
+                #core#
+                cp -r  aurorae/ ~/.local/share
+                cp -r plasma/ ~/.local/share
+                cp -r wallpapers ~/.local/share
+                cp -r Kvantum ~/.config
+                sudo cp -r Flat-Remix-Green /usr/share/icons
+                sudo cp -r sddm/bluelake /usr/share/sddm/themes/
+                plasmapkg2 --type kwinscript -i scripts/kwin-script-tiling/
+                kpackagetool5 -i plasma/look-and-feel/bluelake/
+                #core_end#
+
+                #finish_up#
+                echo "[BluelakeLinuxInstaller]Done....................."
+                clear
+                #finish_up_end#
+
+#Make_Configs#
+                #banner#
+                echo -e "\033[32mWelcom\033[0m \033[34musing\033[0m \033[31mbluelakelinux\033[0m \033[33mflash\033[0m \033[35mscript\033[0m"
+                #banner_end#
+
+                #progress_bar#
+                echo "[########----------------------------------------]"
+                echo "[BluelakeLinuxInstaller]Configuring your plasma workspace..."
+                #progress_bar_end#
+
+                #core#
+                lookandfeeltool -a bluelake --resetLayout
+                #core_end#
+
+                #finish_up#
+                echo "[BluelakeLinuxInstaller]Done....................."
+                clear
+                #finish_up_end#
+
+            #Done#
