271955935b9ef2ad75951cbb61f23a9fe7467dc1
happyeggchen <happyegg.chen@hotmail.com>
2021-03-07 12:34:17 +0800

    remake script

--- install.sh
+++ install.sh
@@ -1,15 +1,27 @@
 #!/bin/bash
+
+#part#
+
     clear
+
         #flash_plasma_theme#
+
             echo -e "\033[32mWelcom\033[0m \033[34musing\033[0m \033[31mbluelakelinux\033[0m \033[33mflash\033[0m \033[35mscript\033[0m"
             echo "-------------Plasma Theme Installation--------------"
             echo -e "\033[31mYOU GOT TO INSTALL A DESKTOP REDSHIFT AND KVANTUM MANAGER ON YOUR PC\033[0m"
             echo "Now we're going to install bluelake kde theme"
             echo "WARNING! YOU GOT A DARK AND LIGHT THEME , BY DEFAULT IS THE DARK ONE"
-            echo "want to install it into your system , Press enter"
-            echo " Or Ctrl+c to install next part"
+            echo "want to install it into your system , type y to install"
+            echo " Or n to install next part"
             echo "----------------------------------------------------"
             read pause
+
+#confirm system#
+
+    read -r -p "start installer? [y/n] " input
+
+          case $input in
+              y)
 
             #installer#
 
@@ -61,7 +73,19 @@
 
                 #finish_up#
                 echo "[BluelakeLinuxInstaller]Done....................."
-                clear
+                clear ;;
                 #finish_up_end#
 
-            #Done#
+#Done#
+
+                n)
+
+                exit 1 ;;
+
+                *)
+
+                echo "Invalid input..." ;;
+
+              esac
+
+#part_end#
